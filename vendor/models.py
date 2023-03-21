9bf341e16c88b72484996a37b0e4586ce0a20c1b
Snehal Deshpande <snehaldeshpande943@gmail.com>
2023-03-21 10:13:46 +0530

    migrations

--- vendor/models.py
+++ vendor/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from accounts.models import User, UserProfile
-
+from accounts.utils import send_notification
 # Create your models here.
 
 
@@ -15,3 +15,25 @@ class Vendor(models.Model):
 
 def __str__(self):
     return self.vendor_name
+
+def save(self, *args, **kwargs):
+    if self.pk is not None:
+        # update
+        orig = Vendor.objects.get(pk=self.pk)
+        if orig.is_approved != self.is_approved:
+            mail_template = 'accounts/emails/admin_approval_email.html'
+            context = {
+                    'user': self.user,
+                    'is_approved' : self.is_approved,
+                }
+            if self.is_approved == True:
+                # Send notification mail
+                mail_subject = 'Congratulations! Your account is approved.'
+                send_notification(mail_subject, mail_template, context)
+            else:
+                # Send notification mail
+                mail_subject = 'We are sorry! You can not publish your food on Foodonline marketplace!'
+                send_notification(mail_subject, mail_template, context)
+
+
+    return super(Vendor, self).save(*args, **kwargs)
