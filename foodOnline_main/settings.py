6411990ea3b2357465287027ef1c073cdc1429a8
Snehal Deshpande <snehaldeshpande943@gmail.com>
2023-04-17 17:04:29 +0530

    restaurant profile and google geocoding

--- foodOnline_main/settings.py
+++ foodOnline_main/settings.py
@@ -67,6 +67,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'accounts.context_processors.get_vendor',
+                'accounts.context_processors.get_google_api',
             ],
         },
     },
@@ -157,3 +158,5 @@ EMAIL_HOST_USER = config('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 EMAIL_USE_TLS = True
 DEFAULT_FROM_MAIL = 'FoodOnline MarketPlace <snehaldeshpande943@gmail.com>'
+
+GOOGLE_API_KEY = 'AIzaSyAFD8sUoWMEFrdfjnIlPjiLTp4TlEdBh4Y'
