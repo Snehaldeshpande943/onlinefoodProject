ee7ff6b3164be667ad14c5fdf17aa0549a90b12d
Snehal Deshpande <snehaldeshpande943@gmail.com>
2023-05-06 15:43:29 +0530

    Update custDashboard

--- accounts/views.py
+++ accounts/views.py
@@ -13,8 +13,10 @@ from django.contrib.auth.decorators import login_required, user_passes_test
 from django.core.exceptions import PermissionDenied
 from vendor.models import Vendor
 from django.template.defaultfilters import slugify
-# Restrict vendor from accessing customer page
+from orders.models import Order
 
+
+# Restrict vendor from accessing customer page
 def check_role_vendor(user):
     if user.role == 1:
         return True
@@ -171,7 +173,15 @@ def myAccount(request):
 @login_required(login_url = 'login')
 @user_passes_test(check_role_customer)
 def custDashboard(request):
-    return render(request, 'accounts/custDashboard.html')
+    orders = Order.objects.filter(user=request.user, is_ordered=True)
+    recent_orders = orders[:5]
+    context = {
+        'orders': orders,
+        'orders_count': orders.count(),
+        'recent_orders': recent_orders,
+    }
+
+    return render(request, 'accounts/custDashboard.html', context)
 
 @login_required(login_url = 'login')
 @user_passes_test(check_role_vendor)
